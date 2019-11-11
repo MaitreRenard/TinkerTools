56e18f2b599680da3899860a168d5756e9943850
Tom MOUNET <Mister0x@protonmail.com>
2019-11-11 21:02:50 +0100

    Mise en place d'un exemple avec argument fonctionnel

--- templates/basis_with_args.c
+++ templates/basis_with_args.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include <argp.h>
 
+int parse_options(int key, char *arg, struct argp_state *state) {
+	switch (key) {
+		case 'd':
+			for (int i=0; i<atoi(arg); i++)
+				printf("%d ",i);
+			printf("\n");
+			break;
+
+		case 'q':
+			printf("Chhhhh\n");
+			break;
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv) {
-	return argp_parse(0, argc, argv, 0, 0, 0);
+	struct argp_option options[] =
+	{
+		{ "quiet", 'q', 0, 0, "Dont show progress or errors." },
+		{ "digit", 'd', "NUMBER", 0, "Print from 0 to NUMBER." },
+		{ 0 }
+	};
+
+	struct argp argp = { options, parse_options };
+
+	return argp_parse(&argp, argc, argv, 0, 0, 0);
 }
