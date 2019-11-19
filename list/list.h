57b68e1b7ac64217c2821336af0b47acdf2471b5
Tom MOUNET <Mister0x@protonmail.com>
2019-11-19 18:08:04 +0100

    Ajout de dossier pour la crypto (b64) pour le moment, merci https://cryptopals.com

--- list/list.h
+++ list/list.h
@@ -1,9 +1,11 @@
 #ifndef _INTEGER_LIST_
 #define _INTEGER_LIST_
 
+#include<stdbool.h>
+
 typedef struct node node;
 struct node {
-	int value;
+	void *value;
 	node *next;
 };
 
@@ -19,5 +21,13 @@ node* newNode();
 
 void printList(list* myList);
 void addToList(int n, list* l);
+
+/*
+* Add value to list
+* @param list The list we add the element to
+* @param value The value to add
+*/
+int add(list* list, void *value);
+int rm(list* list, int index)
 
 #endif
