a42bd8b170856ed2f1c3e742a2b58560a813195a
masterfox <tom.mounet@pm.me>
2020-10-07 00:05:20 +0200

    Progress on jwt parsing

--- jwt/jwt.c
+++ jwt/jwt.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #include <b64.h>
-#include <json.h>
+#include <ccan/json.h>
 #include <openssl/hmac.h>
 
 #include "jwt.h"
@@ -12,26 +12,40 @@
 const char delim[2] = ".";
 const char space[2] = " ";
 
+bool json_exists(JsonNode* json_node, const char* key) {
+	return (json_find_member(json_node, key) != NULL);
+}
+
 jwt_token* parse_jwt(char* encoded_data) {
-	char *token_part, *dec;
-	jwt_token* token = malloc(sizeof(jwt_token));
+	if (strchr(encoded_data, delim[0]) != NULL) {
+		char *token_part, *dec;
+		jwt_token* token = malloc(sizeof(jwt_token));
 
-	token->encoded = encoded_data;
+		token->encoded = encoded_data;
 
-	token_part = strtok(token->encoded, delim);
-	dec = (char*) b64_decode(token_part, strlen(token_part));
-	parse_header(token);
+		token_part = strtok(token->encoded, delim);
+		parse_header(token);
 
-	token_part = strtok(NULL, delim);
-	parse_payload(token);
+		token_part = strtok(NULL, delim);
+		parse_payload(token);
 
-	token_part = strtok(NULL, delim);
-	parse_signature(token);
+		token_part = strtok(NULL, delim);
+		parse_signature(token);
 
+		return token;
+	}
+
+	return NULL;
 }
 
 void parse_header(jwt_token* jtkn) {
 	JsonNode *part_data, *tmp_child;
+	char *dec = (char*) b64_decode(jtkn->encoded, strlen(jtkn->encoded));
+	part_data = json_decode(dec);
+	if (json_exists(part_data,"alg")) {
+		printf("%s\n", json_stringify(json_find_member(part_data, "alg"), space));
+	}
+
 }
 
 void parse_payload(jwt_token* jtkn) {
@@ -40,13 +54,14 @@ void parse_payload(jwt_token* jtkn) {
 void parse_signature(jwt_token* jtkn) {
 }
 
-bool json_exists(JsonNode* json_node, const char* key) {
-	return (json_find_member(json_node, key) != NULL);
-}
-
 int main(int argc, char **argv) {
 	if(argc > 1) {
-
+		jwt_token *token = parse_jwt(argv[1]);
+		if (token) {
+			puts("youpi");
+		} else {
+			puts("Could not parse the JWT token.");
+		}
 		//unsigned char *dec = b64_decode(token_part, strlen(token_part));
 	} else {
 		printf("%s takes at least one argument.\n", argv[0]);
